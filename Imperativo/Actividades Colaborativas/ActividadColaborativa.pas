8a710659757ea46fcd44a03bc0ad493acc9c3578
Joaquin <joaquin.pagani@alu.ing.unlp.edu.ar>
2024-08-28 11:01:30 -0300

    Commit ActividadColaborativa
    Co-Authored-By: Mateo Alfonso <mateo.alfonso@alu.ing.unlp.edu.ar>

--- Imperativo/Actividades Colaborativas/ActividadColaborativa.pas
+++ Imperativo/Actividades Colaborativas/ActividadColaborativa.pas
@@ -6,6 +6,13 @@
 
 
 
+
+
+
+
+
+
+
 {Una empresa de micros de larga distancia gestiona sus ventas. Necesita almacenarla siguiente
 información para sus destinos de viaje: nombre del destino, distancia en kilómetros y la cantidad de pasajes vendidos para dicho destino.
 Dicha información se procesa hasta que se lee el destino “Fin”.
@@ -20,7 +27,7 @@ Dicha información se procesa hasta que se lee el destino “Fin”.
         - Sumar una cantidad X de pasajes a un destino.}
 
 
-Program ActividadColaborativa
+Program ActividadColaborativa;
 
 Type 
 
@@ -49,7 +56,21 @@ Type
 
 
 
-//el arbol es la estructura mas eficiente a la hora de realizar busquedas, el tiempo medio es O(log n)
+
+
+
+
+
+
+
+  //el arbol es la estructura mas eficiente a la hora de realizar busquedas, el tiempo medio es O(log n)
+
+
+
+
+
+
+
 
 
 
@@ -70,14 +91,21 @@ Begin
       a^.HD := Nil;
     End
   Else
-    If a^.dato.nombre > info.nombre Then
+    If (a^.dato.nombre > i.nombre) Then
       insertar(a^.HI, i)
   Else
-    If  a^.dato.mombre < info.nombre Then
-      insertar(a^.HD, i);
+    If  a^.dato.nombre < i.nombre Then
+      insertar(a^.HD, i)
   Else
     writeln('El destino ya fue ingresado');
 End;
+
+
+
+
+
+
+
 
 
 
@@ -93,7 +121,7 @@ Procedure LeerInfo(Var i: info);
 Begin
   Write('Nombre del destino: ');
   readln(i.nombre);
-  If (i.destino <> 'Fin') Then
+  If (i.nombre <> 'Fin') Then
     Begin
       Write('Distancia: ');
       readln(i.distancia);
@@ -103,11 +131,14 @@ Begin
 End;
 
 Procedure cargarEstructura(Var a:arbol);
+
+Var 
+  i: info;
 Begin
   LeerInfo(i);
-  While (i.destion <> 'Fin') Do
+  While (i.nombre <> 'Fin') Do
     Begin
-      insertar(a, info)
+      insertar(a, i);
       LeerInfo(i);
     End;
 End;
@@ -120,10 +151,9 @@ Begin
   insertar(a,i)
 End;
 
-Procedure ImprimirInfo(i);
+Procedure ImprimirInfo(i: info);
 Begin
-  writeln('Destino: 'i.nombre, ' - Dist ', i.distancia, ' - Pasajes vendidos: ',
-          i.pasajesVendidos);
+  writeln('Destino: ', i.nombre, ' - Dist ', i.distancia, ' - Pasajes vendidos: ', i.pasajesVendidos);
 End;
 
 Procedure EnOrden(a:arbol);
@@ -161,15 +191,15 @@ Begin
   write('Ingrese el destino a buscar: ');
   readln(d);
   p := Buscar(a,d);
-  If (p <> Nil) Then ImprimirInfo(a^.dato);
-  Else writeln('No se encontro el destino')
+  If (p <> Nil) Then ImprimirInfo(a^.dato)
+  Else writeln('No se encontro el destino');
 End;
 
 Procedure minimo( a: arbol; Var pmin:arbol);
 Begin
   If ( a <> Nil ) Then
     Begin
-      If (a^.info.canttw < pmin^.dato.distancia) Then
+      If (a^.dato.distancia < pmin^.dato.distancia) Then
         Begin
           pmin := a;
         End;
@@ -177,6 +207,7 @@ Begin
       Minimo(a^.HD,pmin);
     End;
 End;
+
 
 
 Procedure sumarPasajes(a:arbol);
@@ -188,13 +219,55 @@ Begin
   write('Ingrese el destino para agregar los pasajes: ');
   readln(d);
   write('Ingrese la Cantidad de pasajes que quiere sumar: ');
-  readln(sum)
+  readln(sum);
   punta := buscar(a,d);
   punta^.dato.pasajesVendidos := punta^.dato.pasajesVendidos + sum;
 End;
+
 
 
 // Programa Principal
 
 Var 
   i: info;
+  a, pmin: arbol;
+  opcion: integer;
+
+Begin
+  a := Nil;
+  new(pmin);
+  pmin^.dato.distancia := 10000;
+  opcion := 0;
+
+  Repeat
+    writeln('--- MENU ---');
+    writeln('1. Inicializar estructuras de datos');
+    writeln('2. Cargar un destino');
+    writeln('3. Imprimir la informacion');
+    writeln('4. Buscar un destino');
+    writeln('5. Buscar e informar el destino mas cercano');
+    writeln('6. Sumar una cantidad X de pasajes a un destino');
+    writeln('0. Salir');
+    writeln('Ingrese una opcion: ');
+    Writeln();
+    readln(opcion);
+
+    Case opcion Of 
+      1: cargarEstructura(a);
+      2: CargarDestino(a);
+      3: EnOrden(a);
+      4: BuscarDestino(a);
+      5:
+         Begin
+           minimo(a, pmin);
+           writeln('El destino mas cercano es: ');
+           ImprimirInfo(pmin^.dato);
+         End;
+      6: sumarPasajes(a);
+      0: writeln('Saliendo del programa...');
+      Else writeln('Opcion invalida');
+    End;
+
+  Until opcion = 0;
+
+End.
